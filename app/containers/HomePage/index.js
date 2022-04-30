e9d000f1ae3562f303743d7f969e897666a6aab1
Mritunjay Goutam <mritunjaygoutam2204@gmail.com>
2022-05-01 03:30:53 +0530

    Add dev-space option for homepage options

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -5,17 +5,39 @@
  */
 
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 
 import H1 from 'components/H1';
 import messages from './messages';
 
-export default function NotFound() {
+export default function NotFound({match, location}){
+  const search = location.search;
+  const admin = new URLSearchParams(search).get('dev-space');
+  console.log('admin', admin, match, location);
+
   return (
+    
     <article>
       <H1>
         <FormattedMessage {...messages.header} />
       </H1>
+      <hr />
+      {admin && (
+        <span>
+          <Link to="/school">
+        <button type="button">School</button>
+      </Link>
+      <hr />
+      <Link to="/cars">
+        <button type="button">Cars</button>
+      </Link>
+      <hr />
+      <Link to="/dog">
+        <button type="button">Dog</button>
+      </Link>
+        </span>
+      )}
     </article>
   );
 }
